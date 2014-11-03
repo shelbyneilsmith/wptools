ad76e7050d9a1442668e1c3bac5fd7285410b8d0
Shelby Neil Smith <shelby@yellowberri.com>
2014-11-03 11:25:12 -0600

    - commented out check for ruby version for UTF-8 fix in config.rb

--- wordpress/yb/config.rb
+++ wordpress/yb/config.rb
@@ -30,7 +30,7 @@ preferred_syntax = :scss
 
 sass_options = { :debug_info => true }
 
-if RUBY_VERSION =~ /1.9/
+#if RUBY_VERSION =~ /1.9/
 Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
-end
+#end
