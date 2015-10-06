3535d26b38bdca75e426542fa454fe2dea38cb2f
Dalton Rowe <cdaltonrowe@gmail.com>
2015-10-06 12:39:47 -0500

    move yb_layout to utils, add default cot declarations

--- wordpress/yb/functions.php
+++ wordpress/yb/functions.php
@@ -49,191 +49,93 @@ include_once('assets/functions/functions-woocommerce.php');
 /*include_once('assets/inc/widgets/widget-contact.php');*/
 
 /* ------------------------------------------------------------------------ */
-/* REDUX mobile menu classes on body_class() */
+/*     Custom Post Type : book     */
 /* ------------------------------------------------------------------------ */
 
-function yb_mobile_menu_classes( $classes ) {
-  global $ybwp_data;
+/*
+*  Captial, plural      Books
+*  Captial, singular    Book
+*  Lowercase, plural    books
+*  Lowercase, singular   book
+*/
 
-  if ( !empty($ybwp_data['opt-checkbox-mobilemenu'] ) ) {
-    $classes[] = 'mobile-menu';
-
-    if ( $ybwp_data['opt-select-mobilemenutype'] === 'offscreen-nav' ) {
-      $classes[] = 'offscreen-nav';
-      $classes[] = 'mobilenav-pos-'.$ybwp_data['opt-select-offscreenpos'];
-      $classes[] = 'mobilenav-anim-'.$ybwp_data['opt-select-mobilemenuanim'];
-      $classes[] = 'mobilenav-btn-pos-'.$ybwp_data['opt-select-menubtnpos'];
-
-      if ( $ybwp_data['opt-checkbox-utilitynavmerge'] ) { $classes[] = 'util-nav-merge'; }
-    }
-  }
-
-  return $classes;
+function cpt_book_init() {
+  $labels = array(
+    'name'  => 'Books',
+    'singular_name' => 'Book',
+    'menu_name' => 'Books',
+    'name_admin_bar' => 'Book',
+    'add_new'  => 'Add New', 'book',
+    'add_new_item' => 'Add New Book',
+    'new_item'  => 'New Book',
+    'edit_item'  => 'Edit Book',
+    'view_item'  => 'View Book',
+    'all_items' => 'All Books',
+    'search_items' => 'Search Books',
+    'parent_item_colon' => 'Parent Books:',
+    'not_found' => 'No books found.',
+    'not_found_in_trash' => 'No books found in Trash.',
+  );
+ 
+  $args = array(
+    'labels' => $labels,
+    'public' => true,
+    'publicly_queryable' => true,
+    'show_ui' => true,
+    'show_in_menu' => true,
+    'query_var' => true,
+    'rewrite' => array( 'slug' => 'book' ),
+    'capability_type' => 'post',
+    'has_archive' => true,
+    'hierarchical' => true,
+    'menu_position' => null,
+    'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt' )
+  );
+ 
+  register_post_type( 'book', $args );
 }
-add_filter( 'body_class', 'yb_mobile_menu_classes' );
+ 
+//add_action( 'init', 'cpt_book_init' );
 
 /* ------------------------------------------------------------------------ */
-/* Return REDUX page layout and sidebar classes */
+/*     Custom Taxonomy : genre     */
 /* ------------------------------------------------------------------------ */
 
-function yb_layout_class() {
-  global $ybwp_data;
-  global $post;
+/*
+*  Capital, plural      Genres
+*  Capital, singular     Genre
+*  Lowercase, singular   genre
+*  Custom post type    book
+*/
 
-  /* Force full width by page ID */
-  $fullwidth_ids = [];
+function create_book_taxonomies() {
+  // Add new taxonomy, make it hierarchical (like categories)
+  $labels = array(
+    'name' => 'Genres',
+    'singular_name' => 'Genre',
+    'search_items' => 'Search Genres',
+    'all_items' => 'All Genres',
+    'parent_item' => 'Parent Genre',
+    'parent_item_colon' => 'Parent Genre:',
+    'edit_item' => 'Edit Genre',
+    'update_item' => 'Update Genre',
+    'add_new_item' => 'Add New Genre',
+    'new_item_name' => 'New Genre Name',
+    'menu_name' => 'Genres',
+  );
 
-  $post_id = $post->ID;
-  $class_array = [];
-  $return_default = false;
+  $args = array(
+    'hierarchical' => true,
+    'labels' => $labels,
+    'show_ui' => true,
+    'show_admin_column' => true,
+    'query_var' => true,
+    'rewrite' => array( 'slug' => 'genre' ),
+  );
 
-  /* Add Blog Settings to <body> */
-  if ( is_blog() ) {
-
-    /* Set Blog Sidebar */
-    switch($ybwp_data['opt-bloglayout']) {
-      case "Centered Left Sidebar" :
-      $class_array[] = "sidebar-left";
-      break;
-
-      case "Centered Right Sidebar" :
-      $class_array[] = "sidebar-right";
-      break;
-
-      default :
-      $return_default = true;
-      break;
-    }
-  }
-
-  /* Apply Front Page Settings */
-  if ( is_front_page() ) {
-    switch ($ybwp_data['opt-homelayout']) {
-
-      case 'Full Width':
-      $class_array[] = "full-width"; 
-      break;
-
-      case 'Centered Left Sidebar':
-      $class_array[] = "sidebar-left"; 
-      break;
-
-      case 'Centered Right Sidebar':
-      $class_array[] = "sidebar-right"; 
-      break;
-      
-      case 'default':
-      $return_default = true;
-      break;
-
-      default:
-      break;
-    }
-  }
-
-  /* Is if WooCommerce template, use it's settings */
-  if (is_page_template('woocommerce.php')) {
-    switch($ybwp_data['opt-woocommercelayout']) {
-      case "Full Width" :
-      $class_array[] = "full-width";
-      break;
-
-      default :
-      $return_default = true;
-      break;
-    }
-  }
-
-  /* Appy General Layout setting */
-  if ( !is_blog() && !is_front_page() && !is_page_template('woocommerce.php') || $return_default === true ) {
-    switch ($ybwp_data['opt-layout']) {
-      case 'Full Width':
-      $class_array[] = "full-width"; 
-      break;
-
-      case 'Centered Left Sidebar':
-      $class_array[] = "sidebar-left"; 
-      break;
-
-      case 'Centered Right Sidebar':
-      $class_array[] = "sidebar-right"; 
-      break;
-
-      default:
-      /* Its the default! No class needed. */
-      break;
-    }
-  }
-  
-  /* Force full width by page ID */
-  if (in_array($post_id, $fullwidth_ids)) {
-    $class_array[] = "full-width";
-  }
-
-  return $class_array;
+  register_taxonomy( 'genre', array( 'book' ), $args );
 }
 
-/* ------------------------------------------------------------------------ */
-/* Blog layout post classes on body_class() */
-/* ------------------------------------------------------------------------ */
-
-function yb_blog_layout_class() {
-  global $ybwp_data;
-  $class_array = [];
-
-  if (is_blog()) {
-    switch($ybwp_data['opt-select-blogpostlayout']) {
-      case "blog-medium" :
-      $class_array[] = "blog-medium";
-      break;
-
-      case "blog-fullwidth" :
-      default :
-      $class_array[] = "blog-fullwidth";
-      break;
-    }
-  }
-
-  return $class_array;
-}
-
-/* ------------------------------------------------------------------------ */
-/* Add yb_layout_classes to body_class() */
-/* requires yb_layout_class() */
-/* ------------------------------------------------------------------------ */
-
-function yb_body_class( $classes ) {
-  /* Combine yb_layout_class array with body classes */
-  $page_classes = yb_layout_class();
-  $classes = array_merge($classes, $page_classes);
-
-  /* Combine blog layout classes */
-  //$blog_classes = yb_blog_layout_class();
-  //$classes = array_merge($classes, $blog_classes);
-
-  return $classes;
-}
-add_filter( 'body_class', 'yb_body_class' );
-
-/* ------------------------------------------------------------------------ */
-/* is_sidebar_page() : If this page should have a sideber */
-/* requires yb_layout_class() */
-/* ------------------------------------------------------------------------ */
-
-function is_sidebar_page() {
-
-  $is_sidebar_page = false;
-  $sidebar_classes = ['sidebar-right', 'sidebar-left'];
-  $page_classes = yb_layout_class();
-
-  foreach ($sidebar_classes as $class) {
-    if ( in_array($class, $page_classes) ) { 
-      $is_sidebar_page = true; 
-    }
-  }
-
-  return $is_sidebar_page;
-}
-
+//add_action( 'init', 'create_book_taxonomies', 0 );
 
 ?>
